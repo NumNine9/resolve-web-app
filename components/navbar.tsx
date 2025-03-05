3730042540435744304dae438920a49bd55db13a
George Nyagura <gnyagura57@gmail.com>
2025-03-05 21:28:21 +0100

    Navbar component changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -76,7 +76,7 @@ export const Navbar = () => {
             {/* <Logo /> */}
             <p className="font-bold text-inherit">Resolve</p>
           </NextLink>
-        </NavbarBrand>
+        </NavbarBrand> 
         <div className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
