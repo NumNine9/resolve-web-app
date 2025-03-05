e593e56b51439c4fbeeeb2ca5ba3b90dac1f041b
George Nyagura <gnyagura57@gmail.com>
2025-03-05 21:26:47 +0100

    Added default theme to web- app

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -3,17 +3,19 @@ import { Link } from "@heroui/link";
 import { Head } from "./head";
 
 import { Navbar } from "@/components/navbar";
+import { useTheme } from "next-themes";
 
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { theme } = useTheme(); // Get the current theme
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className={`${theme} relative flex flex-col h-screen`}>
       <Head />
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className={`${theme} container mx-auto max-w-7xl px-6 flex-grow pt-16`}>
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3">
